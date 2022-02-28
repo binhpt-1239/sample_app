ad88cb12b697f8233135e7daec024deeccd79094
binhpt-1239 <pham.thanh.binh@sun-asterisk.com>
2022-02-28 21:47:28 +0700

    Chapter3_4_5: Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    d_local = I18n.default_locale
+    I18n.locale = I18n.available_locales.include?(locale) ? locale : d_local
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/log_in"
+    get "static_pages/log_up"
+  end
 end
